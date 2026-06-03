a5dc6568fbdcd3788e5d6c62b9ee82c69681eb99
Petro Kravchenko <petros5325@gmail.com>
2026-06-03 17:56:31 +0300

    Fix name and save all

--- task.sql
+++ task.sql
@@ -1,16 +1,22 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
 
-create index idx_products_name on Products (Name);
-create index idx_products_price on Products (Price);
-create index idx_products_amount on Products (WarehouseAmount);
+-- Таблиця Products
+CREATE INDEX Name ON Products (Name);
+CREATE INDEX Description ON Products (Description);
+CREATE INDEX Price ON Products (Price);
+CREATE INDEX WarehouseAmount ON Products (WarehouseAmount);
 
-create index idx_customers_FirstName on Customers (FirstName);
-create index idx_customers_LastName on Customers (LastName);
-create index idx_customers_email on Customers (Email);
+-- Таблиця Customers
+CREATE INDEX FirstName ON Customers (FirstName);
+CREATE INDEX LastName ON Customers (LastName);
+CREATE INDEX Email ON Customers (Email);
+CREATE INDEX Address ON Customers (Address);
 
-create index idx_orders_customerID on Orders (CustomerID);
-create index idx_orders_date on Orders (Date);
+-- Таблиця Orders
+CREATE INDEX CustomerID ON Orders (CustomerID);
+CREATE INDEX Date ON Orders (Date);
 
-create index idx_orderItems_orderID on OrderItems (OrderID);
-create index idx_orderItems_productID on OrderItems (ProductID);
+-- Таблиця OrderItems
+CREATE INDEX OrderID ON OrderItems (OrderID);
+CREATE INDEX ProductID ON OrderItems (ProductID);
